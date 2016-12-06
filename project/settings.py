e674292bfbc46fc1914f3fb609da9372b8db042d
abhiramrk <a.ravikumar@sap.com>
2016-12-06 12:16:45 +0530

    Adds Fb login button trial 4

--- project/settings.py
+++ project/settings.py
@@ -110,3 +110,13 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(
+        os.path.dirname(__file__),
+        'static',
+    ),
+)
