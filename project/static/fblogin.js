486f831ffa032550165346e8689b35c1802dff3c
Kulkarni <I331582@global.corp.sap>
2016-12-10 15:31:45 +0530

    console of response 3

--- project/static/fblogin.js
+++ project/static/fblogin.js
@@ -3,7 +3,7 @@ console.log("Fb login script loaded")
   // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
     console.log('statusChangeCallback');
-    console.log(response);
+  //  console.log(response);
     // The response object is returned with a status field that lets the
     // app know the current login status of the person.
     // Full docs on the response object can be found in the documentation
@@ -78,4 +78,45 @@ console.log("Fb login script loaded")
         //'Thanks for logging in, ' + response.name + '!';
     });
   }
+// NEW CODE
 
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1737527669900907',
+       cookie     : true,
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    FB.getLoginStatus(function(response){
+      if (response.status === 'connected'){
+        document.getElementById('status').innerHTML = 'We are connected';
+      }
+      else if(response.status === 'not_authorized'){
+        document.getElementById('status').innerHTML = 'We are not connected';
+      }
+      else{
+        document.getElementById('status').innerHTML = 'you are not logged into facebook';
+      }
+    });
+  };
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+  function login() {
+    FB.login(function(response){
+      if (response.status === 'connected'){
+          document.getElementById('status').innerHTML = 'We are connected';
+        }
+        else if(response.status === 'not_authorized'){
+          document.getElementById('status').innerHTML = 'We are not connected';
+        }
+        else{
+          document.getElementById('status').innerHTML = 'You are not logged into facebook';
+        }
+    },{scope:'user_likes'});
+  }
