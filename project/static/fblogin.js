8b519e2f30ea1634d76e834f0645759dc8b695d5
Kulkarni <I331582@global.corp.sap>
2016-12-11 16:02:56 +0530

    console of response 6

--- project/static/fblogin.js
+++ project/static/fblogin.js
@@ -72,8 +72,9 @@ console.log("Fb login script loaded")
   // successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
+    FB.api('/me', {fields: 'gender, first_name, last_name, email, likes'}, function(response) {
+      console.log('Successful login for: ' + response.name+" "+response.email);
+      console.log(response);
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
     });
